dd9e2671b26b0af1a35ce783be79169eaea73efd
jezzixxx <pkapav02@gmail.com>
2023-03-08 02:47:49 +0300

    fixed hello_world.cpp with comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,13 @@
 #include <string>
 
 int main(){
+	//Выводим на экран запрос о вводе имени
 	std::cout<<"Input your name:";
+	//Создаём переменную name типа std::string
 	std::string name;
+	//Вводим с клавиатуры переменную name
 	std::cin>>name;
+	//Выводим на экран надпись "Hello world from @name", где name - введённое на предыдущем шаге имя
 	std::cout<<"Hello world from "<<name<<std::endl;
 	return 0;
 }
